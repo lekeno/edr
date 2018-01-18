f607ad5459fcf04cd1a41094d037fba501846205
LeKeno <33626494+lekeno@users.noreply.github.com>
2018-01-18 12:58:34 +0900

    Changes to the CmdrsDex
    In preparation of the EDR backend, see issue #55

--- edr/edrcmdrsdex.py
+++ edr/edrcmdrsdex.py
@@ -1,10 +1,13 @@
 import datetime
+import time
+import calendar
 import json
 import os
+import edtime
 
 class EDRCmdrsDex(object):
     EDR_CMDRS_DEX_PATH = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), 'data/cmdrsdex.json')
+        os.path.abspath(os.path.dirname(__file__)), 'data/cmdrsdex.0.5.0.beta.3.json')
 
     def __init__(self):
         try:
@@ -15,74 +18,132 @@ class EDRCmdrsDex(object):
     
     def get(self, cmdr_name):
         return self.cmdrs.get(cmdr_name.lower(), None)
+
+    def __all_tags(self, cmdr_name):
+        if not self.exists(cmdr_name):
+            return None
+        
+        all_tags = []
+        cname = cmdr_name.lower()
+        if self.cmdrs[cname]["alignment"]:
+            all_tags.append(self.cmdrs[cname]["alignment"])
+        
+        if self.cmdrs[cname]["tags"]:
+            all_tags += self.cmdrs[cname]["tags"]
+        return all_tags
     
     def short_profile(self, cmdr_name):
-        cmdr_entry = self.get(cmdr_name)
-        if cmdr_entry:
-            return u"{}: {} #{} ({})".format(cmdr_entry["name"], cmdr_entry["memo"], " ".join(cmdr_entry["tags"]), cmdr_entry["date"])
-        return None
+        if not self.exists(cmdr_name):
+            return None
 
-    def add(self, name, memo, tag):
-        tag = tag.lower()
-        self.remove(name)
+        cname = cmdr_name.lower()
+        profile = u"{}".format(self.cmdrs[cname]["name"])
+        
+        if self.cmdrs[cname]["friend"]:
+            profile += u" [friend]"
+
+
+        all_tags = self.__all_tags(cmdr_name)
+        if all_tags:
+            profile += u" #{}".format(" #".join(all_tags))
+        
+        memo = self.cmdrs[cname]["memo"]
+        if memo:
+            profile += u" {}".format(memo)
+
+        updated = self.cmdrs[cname]["updated"]
+        if updated:
+            ed_updated = edtime.EDTime()
+            ed_updated.from_js_epoch(updated)
+            profile += u" ({})".format(ed_updated.as_immersive_date())
+        
+        return profile
+
+    def add(self, name):
+        if self.exists(name):
+            return False
+
+        now = EDRCmdrsDex.__js_epoch_now()
         cmdr_entry = {
             "name": name,
-            "memo": memo,
-            "tags": [tag],
-            "date": datetime.datetime.today().strftime('%Y-%m-%d')
+            "alignment": None,
+            "tags": [],
+            "friend": None,
+            "memo": None,
+            "created": now,
+            "updated": now
         }
         self.cmdrs[name.lower()] = cmdr_entry
     
-    def set_memo(self, name, memo):
-        cmdr_entry = self.get(name)
-        if cmdr_entry:
-            cmdr_entry["memo"] = memo
-            self.cmdrs[name.lower()] = cmdr_entry
-            #TODO EDRServer update
-            return True
-        return False
+    @staticmethod 
+    def alignments():
+        return ["outlaw", "neutral", "enforcer"]
 
-    def tag(self, name, tag):
+    @staticmethod
+    def __js_epoch_now():
+        return 1000 * calendar.timegm(time.gmtime())
+    
+    def exists(self, name):
+        return name.lower() in self.cmdrs
+
+    def memo(self, name, memo):
+        if not self.exists(name):
+            self.add(name)
+
+        cname = name.lower()
+        self.cmdrs[cname]["memo"] = memo
+        self.cmdrs[cname]["updated"] = EDRCmdrsDex.__js_epoch_now()
+        #TODO EDRServer update
+    
+    def remove_memo(self, name):
+        if not self.exists(name):
+            self.add(name)
+
+        cname = name.lower()
+        self.cmdrs[cname]["memo"] = None
+        self.cmdrs[cname]["updated"] = EDRCmdrsDex.__js_epoch_now()
+        #TODO EDRServer update
+
+    @staticmethod
+    def __tagify(tag):
         tag = tag.lower()
-        cmdr_entry = self.get(name)
-        if cmdr_entry is None:
-            self.add(name, None, tag)
-            return True
+        return tag.replace(" ", "")
+    
+    def tag(self, name, tag):
+        if not self.exists(name):
+            self.add(name)
+        
+        cname = name.lower()
+        tag = EDRCmdrsDex.__tagify(tag)
 
-        if tag not in ["outlaw", "neutral", "enforcer"]:
-            cmdr_entry["tags"].append(tag)
-            self.cmdrs[name.lower()] = cmdr_entry
-            return True
-
-        if tag in cmdr_entry["tags"]:
-            return True
-
-        for karma_tag in ["outlaw", "neutral", "enforcer"]:
-            try:
-                cmdr_entry["tags"].remove(karma_tag)
-            except:
-                pass
-
-        cmdr_entry["tags"].append(tag)
-        self.cmdrs[name.lower()] = cmdr_entry
-        return True
+        if tag == "friend":
+            self.cmdrs[cname]["friend"] = True
+        elif tag in EDRCmdrsDex.alignments():
+            self.cmdrs[cname]["alignment"] = tag
+        else:
+            self.cmdrs[cname]["tags"].append(tag)
+        
+        self.cmdrs[cname]["updated"] = EDRCmdrsDex.__js_epoch_now()
+        #TODO EDRServer update
 
     def untag(self, name, tag):
-        tag = tag.lower()
-        cmdr_entry = self.get(name)
-        if cmdr_entry is None:
-            return False
+        if not self.exists(name):
+            return
+        
+        cname = name.lower()
+        tag = EDRCmdrsDex.__tagify(tag)
 
-        if tag not in cmdr_entry["tags"]:
-            return True
-
-        cmdr_entry["tags"].remove(tag)
-        if cmdr_entry["tags"]:
-            self.cmdrs[name.lower()] = cmdr_entry
+        if tag == "friend":
+            self.cmdrs[cname]["friend"] = False
+        elif tag == self.cmdrs[cname]["alignment"]:
+             self.cmdrs[cname]["alignment"] = None
         else:
-            self.remove(name)
-        return True
-
+            if tag not in self.cmdrs[cname]["tags"]:
+                return
+            self.cmdrs[cname]["tags"].remove(tag)
+        
+        self.cmdrs[cname]["updated"] = EDRCmdrsDex.__js_epoch_now()
+        #TODO EDRServer update
     
     def remove(self, name):
         try:
--- edr/edtime.py
+++ edr/edtime.py
@@ -6,7 +6,14 @@ import comparable
 class EDTime(object, comparable.ComparableMixin):
     @staticmethod
     def immersive_delta():
-        return 11644473600 # immersive time set 369 years in the future
+        return 1286 # Elite Dangerous is set 1286 years in the future
+
+    def __immmersive(self):
+        d = self._datetime
+        try:
+            return d.replace(year = d.year + EDTime.immersive_delta())
+        except ValueError:
+            return d + (date(d.year + EDTime.immersive_delta(), 1, 1) - date(d.year, 1, 1))
 
     def __init__(self):
         self._datetime = datetime.datetime.now()
@@ -14,11 +21,11 @@ class EDTime(object, comparable.ComparableMixin):
     def from_datetime(self, datetimestamp):
         self._datetime = datetimestamp
 
+    def from_js_epoch(self, js_epoch):
+        self._datetime = datetime.datetime.fromtimestamp(js_epoch / 1000)
+
     def from_journal_timestamp(self, journal_timestamp):
         self._datetime = datetime.datetime.strptime(journal_timestamp, '%Y-%m-%dT%H:%M:%SZ')
-
-    def from_immersive_epoch(self, epoch):
-        self._datetime = datetime.datetime.utcfromtimestamp(epoch - EDTime.immersive_delta())
     
     def as_js_epoch(self):
         return int(calendar.timegm(self._datetime.timetuple()) * 1000) # JavaScript expects milliseconds while Python uses seconds for Epoch
@@ -32,9 +39,10 @@ class EDTime(object, comparable.ComparableMixin):
     def as_journal_timestamp(self):
         return self._datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
         
-    def as_immersive_epoch(self):
-        epoch = self.as_py_epoch()
-        return epoch + EDTime.immersive_delta()
+    def as_immersive_date(self):
+        immersive_datetime = self.__immmersive()
+        return immersive_datetime.strftime('%Y-%m-%d')
+
 
     def __lt__(self, other):
         if isinstance(other, EDTime):
