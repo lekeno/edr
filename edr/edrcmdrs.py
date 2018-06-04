f675f11c0098fa8bfff783bb8c76ae754fa86c43
LeKeno <33626494+lekeno@users.noreply.github.com>
2018-06-04 22:06:19 +0900

    bump cache versions
    See issue #143

--- edr/edrcmdrs.py
+++ edr/edrcmdrs.py
@@ -12,9 +12,9 @@ EDRLOG = edrlog.EDRLog()
 class EDRCmdrs(object):
     #TODO these should be player and/or squadron specific
     EDR_CMDRS_CACHE = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), 'cache/cmdrs.v4.p')
+        os.path.abspath(os.path.dirname(__file__)), 'cache/cmdrs.v5.p')
     EDR_INARA_CACHE = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), 'cache/inara.v4.p')
+        os.path.abspath(os.path.dirname(__file__)), 'cache/inara.v5.p')
     EDR_SQDRDEX_CACHE = os.path.join(
         os.path.abspath(os.path.dirname(__file__)), 'cache/sqdrdex.v1.p')
 
