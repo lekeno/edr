44e8ea5bc501ab7132b2305f1fa9f3b88614c261
LeKeno <33626494+lekeno@users.noreply.github.com>
2019-09-01 14:15:41 +0900

    Backoff strategy
    See issue #249

--- edr/edrinara.py
+++ edr/edrinara.py
@@ -5,6 +5,8 @@ import requests
 import edrcmdrprofile
 import edrconfig
 import edrlog
+import random
+from edtime import EDTime
 
 EDRLOG = edrlog.EDRLog()
 
@@ -18,6 +20,7 @@ class EDRInara(object):
         self.INARA_API_KEY = config.inara_api_key()
         self.INARA_ENDPOINT = config.inara_endpoint()
         self.requester = None
+        self.backoff_until = 0 
 
     def squadron(self, cmdr_name):
         if self.requester is None:
@@ -45,6 +48,10 @@ class EDRInara(object):
     def __cmdr(self, cmdr_name):
         payload = { "header": self.__api_header(), "events" : [self.__api_cmdrprofile(cmdr_name)] }
         resp = None
+        if self.__should_hold_back():
+            EDRLOG.log(u"Backing off from calling Inara until {} (now={})".format(self.backoff_until, EDTime.py_epoch_now()), "DEBUG")
+            return None
+
         try:
             resp = EDRInara.SESSION.post(self.INARA_ENDPOINT, json=payload)
             if resp.status_code != requests.codes.ok:
@@ -57,14 +64,20 @@ class EDRInara(object):
         EDRLOG.log(u"Obtained a response from the Inara API.", "INFO")
         try:
             json_resp = json.loads(resp.content)
+            if json_resp["header"]["eventStatus"] == 400:
+                EDRLOG.log(u"Too much requests for Inara.", "INFO")
+                self.__backoff()
+                return None
             if json_resp["events"][0]["eventStatus"] == 204:
                 EDRLOG.log(u"cmdr {} was not found via the Inara API.".format(cmdr_name), "INFO")
                 return None
             elif json_resp["events"][0]["eventStatus"] != requests.codes.ok:
                 EDRLOG.log(u"Error from Inara API. code={code}, content={content}".format(code=resp.status_code, content=resp.content), "ERROR")
+                self.__backoff()
                 return None
         except:
-            EDRLOG.log(u"Malformed cmdr profile response from Inara API? code={code}, content={content}".format(code=resp.status_code, content=resp.content), "ERROR")
+            EDRLOG.log(u"Malformed response from Inara API? code={code}, content={content}".format(code=resp.status_code, content=resp.content), "ERROR")
+            self.__backoff()
             return None
 
         try:
@@ -72,6 +85,7 @@ class EDRInara(object):
             return json_resp
         except:
             EDRLOG.log(u"Malformed cmdr profile response from Inara API? code={code}, content={content}".format(code=resp.status_code, content=resp.content), "ERROR")
+            self.__backoff()
             return None
 
     def __api_header(self):
@@ -91,3 +105,19 @@ class EDRInara(object):
                 "searchName": cmdr_name
             }
         }
+
+    def __backoff(self):
+        base = 10
+        cap = 60 * 60 * 2
+        self.attempt += 1
+        self.backoff_until = EDTime.py_epoch_now() + min(cap, base * 2 ** attempt) + random.randint(0, 60)
+        EDRLOG.log(u"Exponential backoff for Inara API calls: attempts={}, until={}".format(self.attempt, self.backoff_until), "DEBUG")
+
+    def __should_hold_back(self):
+        hold = EDTime.py_epoch_now() < self.backoff_until
+        if not hold:
+            self.attempt = 0
+            self.backoff_until = 0
+        return hold
+
+
