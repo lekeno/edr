4e077871b88175a1acd71d0a38caf6ff0726975a
LeKeno <33626494+lekeno@users.noreply.github.com>
2020-08-22 18:01:27 +0900

    Retry FleetCarrier
    See issue #337

--- edr/lrucache.py
+++ edr/lrucache.py
@@ -15,10 +15,13 @@ class LRUCache(object):
         self.last_updated = None
     
     def is_stale(self, key):
+        return self.is_older_than(key, self.max_age)
+
+    def is_older_than(self, key, age):
         if not self.has_key(key):
             return True
         entry = self.cache[key]
-        return (datetime.datetime.now() - entry["datetime"]) > self.max_age
+        return (datetime.datetime.now() - entry["datetime"]) > age
 
     def values(self):
         return self.cache.values()
