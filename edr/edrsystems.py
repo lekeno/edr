938744de99d7157f0544306a34424c428b712532
LeKeno <33626494+lekeno@users.noreply.github.com>
2020-08-22 19:17:40 +0900

    Revert "typo"
    This reverts commit b60dc3064eb9f13e92b2cfd1780219c24e51ac90.

--- edr/edrsystems.py
+++ edr/edrsystems.py
@@ -188,7 +188,7 @@ class EDRSystems(object):
                 return station
         
         worth_retrying_age = 60*60*6 
-        if station_type == "FleetCarrier" and self.edsm_stations_cache.is_older_than(star_system.lower(), worth_retrying_age):
+        if station_type == "FleetCarrier" and self.edsm_stations_cache.is_older_than(star_system.lower(), age_worth_retrying):
             # FleetCarrier are a bit more dynamic, so evict a lukewarm entry and get a new fresh one in case the info has been reflected since last time
             self.edsm_stations_cache.evict(star_system.lower())
             stations = self.stations_in_system(star_system)
