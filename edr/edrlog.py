691bf9f6ed51d7e7469d41d9453cc723b3c143ef
LeKeno <33626494+lekeno@users.noreply.github.com>
2020-10-21 19:43:45 +0900

    Update edrlog.py
    See issue #342

--- edr/edrlog.py
+++ edr/edrlog.py
@@ -1,7 +1,9 @@
-from __future__ import print_function
 from __future__ import absolute_import
 
+import logging
 import sys
+import os
+from config import appname
 from edrconfig import EDRConfig
 
 if sys.version_info.major == 3:
@@ -9,25 +11,22 @@ if sys.version_info.major == 3:
 
 class EDRLog(object):
 
-    LEVEL_MAPPING = {"DEBUG": 10,  "INFO": 20, "WARNING": 30, "ERROR": 40}
+    LEVEL_MAPPING = {"DEBUG": logging.DEBUG,  "INFO": logging.INFO, "WARNING": logging.WARNING, "ERROR": logging.ERROR, "CRITICAL": logging.CRITICAL}
+    PLUGIN_NAME = os.path.basename(os.path.dirname(__file__))
 
     def __init__(self):
         config = EDRConfig()
-        self.importance_threshold = self.LEVEL_MAPPING.get(config.logging_level(), 0)
+        self.logger = logging.getLogger(f'{appname}.{self.PLUGIN_NAME}')
+        level = self.LEVEL_MAPPING.get(config.logging_level(), logging.NOTSET)
+        self.logger.setLevel(level)
+        if not self.logger.hasHandlers():
+            logger_channel = logging.StreamHandler()
+            logger_formatter = logging.Formatter(f'%(asctime)s - %(name)s - %(levelname)s - %(module)s:%(lineno)d:%(funcName)s: %(message)s')
+            logger_formatter.default_time_format = '%Y-%m-%d %H:%M:%S'
+            logger_formatter.default_msec_format = '%s.%03d'
+            logger_channel.setFormatter(logger_formatter)
+            self.logger.addHandler(logger_channel)
 
     def log(self, msg, level):
-        if not self.is_important_enough(level):
-            return
-
-        if sys.version_info.major == 2:
-            print("[EDR]" + msg.encode(sys.getdefaultencoding(), 'replace'))
-        else:
-            print(u"[EDR]" + msg)
-
-
-    def is_important_enough(self, level):
-        importance = self.LEVEL_MAPPING.get(level, None)
-        if importance is None:
-            return self.importance_threshold == 0
-
-        return importance >= self.importance_threshold
+        level = self.LEVEL_MAPPING.get(level, logging.NOTSET)
+        self.logger.log(level, msg)
