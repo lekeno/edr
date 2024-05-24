7aef0c328e2a736e9a59c6492c75cf097f8d6a9f
Giampiero <54741923+GLWine@users.noreply.github.com>
2024-05-24 07:30:45 +0200

    Enhance language selection mechanism in i18n.py
    Enhancement of language selection mechanism
    
    This commit refines the language selection process within the plugin to ensure better compatibility with the default language setting in Elite Dangerous Market Connector (EDMC).
    
    Changes introduced:
    - Improved language detection: Previously, if EDMC was set to the default language, the plugin failed to populate the `language` variable, resulting in the inability to determine the language. The plugin now utilizes the `l10n.Locale.preferred_languages()` function from EDMC to obtain the user's preferred system language. It verifies if this language is available in the translated language files and assigns the variable accordingly, falling back to `None` if not found.
    
    - Enhanced error handling: The code has been adjusted to address type misuse warnings flagged by `mypy`. A `try-except` block has been added to handle potential errors gracefully, preventing the plugin from crashing in case of unexpected issues. Additionally, specific error types are now caught to ensure proper error detection without obscuring unrelated errors.
    
    - Code organization: The codebase has been restructured to adhere to the PEP 8 style guidelines, improving readability and maintainability.
    
    These changes streamline the language selection process, ensuring better compatibility with user preferences while improving error handling and code clarity.
    
    Resolves: lekeno/edr#487

--- edr/edri18n.py
+++ edr/edri18n.py
@@ -3,32 +3,59 @@
 from __future__ import absolute_import
 
 import gettext
-import os
+# import os # 'os' imported but unused Flake8(F401)
 import sys
 
+import l10n  # Importing localization module
 import utils2to3
+from edrlog import EDRLog  # Importing EDRLog module for logging
 
+# Constants
+CONTEXT_SEPARATOR = u"|"  # Separator used for contextual message
+L10N_DIR = utils2to3.abspathmaker(__file__, 'l10n')  # Directory for localization files
+LANG_LIST = ["fr", "it", "de"]  # List of supported languages
 
-CONTEXT_SEPARATOR = u"|"
-L10N_DIR = utils2to3.abspathmaker(__file__, 'l10n')
+# Logger
+EDRLOG = EDRLog()  # Creating an instance of EDRLog for logging
+
+# Global variables
 language = None
-translate = gettext.translation('edr', L10N_DIR, fallback=True)
+translate = gettext.translation('edr', L10N_DIR, fallback=True)  # Translation object
 
-def set_language(lang):
+try:
+    # Attempting to get preferred languages from l10n module
+    pref_langs = list(l10n.Locale.preferred_languages())
+    # Selecting the first preferred language if available and supported
+    select_lang = pref_langs[0][:-3] if pref_langs and pref_langs[0][:-3] in LANG_LIST else None
+except AttributeError as e:
+    # Handling error when the attribute preferred_languages is not available
+    EDRLOG.log(u"AttributeError: {}".format(e), "WARNING")
+    select_lang = None
+
+
+def set_language(lang):  # Function to set the language for translation
     global language, translate
     language = lang
     if language:
         translate = gettext.translation('edr', L10N_DIR, fallback=True, languages=[language])
+        EDRLOG.log(u"pass 1: lang {}".format(language), "INFO")  # Logging language selection
+    elif select_lang:
+        language = select_lang
+        translate = gettext.translation('edr', L10N_DIR, fallback=True, languages=[language])
+        EDRLOG.log(u"pass 2: lang {}".format(language), "INFO")  # Logging language selection
     else:
         translate = gettext.translation('edr', L10N_DIR, fallback=True)
+        EDRLOG.log(u"Fail: lang en", "INFO")  # Logging failure to select language
 
-def ugettext(message):
+
+def ugettext(message):  # Function to translate a message
     if sys.version_info.major == 2:
         return translate.ugettext(message)
     else:
         return translate.gettext(message)
 
-def pgettext(contextual_message):
+
+def pgettext(contextual_message):  # Function to translate a contextual message
     global translate
     result = None
     if sys.version_info.major == 2:
@@ -36,20 +63,22 @@ def pgettext(contextual_message):
     else:
         result = translate.gettext(contextual_message)
     if CONTEXT_SEPARATOR in result:
-        # Translation not found
+        # Translation not found, returning the default message
         result = contextual_message.split(u"|")[1]
     return result
 
-def edrgettext(message_maybe_localized):
+
+def edrgettext(message_maybe_localized):  # Function to get a localized message
     global language
     try:
         return message_maybe_localized[language]
-    except:
+    except KeyError:
         if "default" in message_maybe_localized:
             return message_maybe_localized["default"]
         else:
             return message_maybe_localized
 
-_ = ugettext
-_c = pgettext
-_edr= edrgettext
+
+_ = ugettext  # Alias for ugettext
+_c = pgettext  # Alias for pgettext
+_edr = edrgettext  # Alias for edrgettext
