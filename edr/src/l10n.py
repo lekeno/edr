4dabb1f72da4d933b3eae4f70230349a10afcee2
lekeno <33626494+lekeno@users.noreply.github.com>
2026-05-10 06:27:14 +0900

    dummy file

--- edr/src/l10n.py
+++
@@ -1,6 +0,0 @@
-class Translations:
-    def gettext(self, text): return text
-def Translations_fallback(*args, **kwargs): return Translations()
-class Locale:
-    @staticmethod
-    def preferred_languages(): return ["en"]
